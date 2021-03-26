5306f2c14e713acfc7723a6556bee2e7bc046ed7
Neil Resnik <neilr22@comcast.net>
2021-03-26 16:36:15 -0400

    Add argon2 + seeds

--- event_app_server/priv/repo/seeds.exs
+++ event_app_server/priv/repo/seeds.exs
@@ -9,3 +9,25 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias EventAppServer.Repo
+alias EventAppServer.Users.User
+alias EventAppServer.Events.Event
+
+defmodule Inject do
+  def user(name, email, pass) do
+    hash = Argon2.hash_pwd_salt(pass)
+    Repo.insert!(%User{name: name, email: email, password_hash: hash})
+  end
+end
+
+alice = Inject.user("alice", "alice@gmail.com", "test1")
+bob = Inject.user("bob", "bob@gmail.com", "test2")
+
+event1 = %Event{
+  name: "Alice's Birthday Bash",
+  date: ~D[2023-08-17],
+  description: "There will be pizza!",
+  user_id: alice.id
+}
+Repo.insert(event1);
